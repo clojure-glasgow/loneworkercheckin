aab0ecd7898583c6d6fb5cad4d8a547ccae83aa0
Robin Gower <robin.git@infonomics.ltd.uk>
2015-02-04 19:12:50 +0000

    Robin/Stef Basic test

--- test/checkin/core_test.clj
+++ test/checkin/core_test.clj
@@ -2,6 +2,8 @@
   (:require [clojure.test :refer :all]
             [checkin.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest shouldReturn200
+  (let [response (home {})]
+    (testing "success"
+      (is (= 200 (:status response))))))
+
