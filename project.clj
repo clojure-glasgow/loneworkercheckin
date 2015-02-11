c2d18c0b781ed928675f031fb7a9242501f61db0
Jonathan Abourbih <jabourbi@thoughtworks.com>
2015-02-11 18:23:02 +0000

    Jonathan/Tony/David/Andy Add midje to project.clj

--- project.clj
+++ project.clj
@@ -12,9 +12,12 @@
                            [cheshire "5.4.0"]
                            ]
 
-            :profiles {:proxied
-                       {:jvm-opts ["-Dhttp.proxyHost=127.0.0.1" "-Dhttp.proxyPort=8888" "-Dhttps.proxyHost=127.0.0.1" "-Dhttps.proxyPort=8888"]}}
-            :plugins [[lein-ring "0.9.1"]]
+            :profiles {:dev  {:dependencies  [[midje "1.6.3"]]}
+                       :proxied
+                       {:jvm-opts ["-Dhttp.proxyHost=127.0.0.1" "-Dhttp.proxyPort=8888"
+                                   "-Dhttps.proxyHost=127.0.0.1" "-Dhttps.proxyPort=8888"]}}
+            :plugins [[lein-ring "0.9.1"]
+                      [lein-midje "3.1.3"]]
             :ring {:handler checkin.handler/app
                    :uberwar-name "loneworkercheckin.war"}
             :aliases {"run-proxied" ["with-profile" "proxied" "ring" "server"]})
