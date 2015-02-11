95f0fd2bc322e990684765381b79d712cdd345f0
Jonathan Abourbih <jabourbi@thoughtworks.com>
2015-02-11 19:40:47 +0000

    Jonathan/Tony Rename test handler

---
+++ test/checkin/handler_test.clj
@@ -0,0 +1,8 @@
+(ns checkin.handler-test
+  (:require [midje.sweet :refer :all]
+            [checkin.handler :refer :all]))
+
+(facts "about http response codes"
+       (fact "should return 200 on home page"
+             (:status (app {:uri "/"
+                   :request-method :get})) => 200))
