202b1cb927f7bc40a80e18a6b579658bd3064bec
Jonathan Abourbih <jabourbi@thoughtworks.com>
2015-02-11 18:26:59 +0000

    Jonathan/Tony Remove tests
    Removing test code

--- test/checkin/core_test.clj
+++ test/checkin/core_test.clj
@@ -1,8 +1,8 @@
 (ns checkin.core-test
   (:require [clojure.test :refer :all]
-            [checkin.core :refer :all]))
+            [checkin.handler :refer :all]))
 
-(deftest shouldReturn200
+#_(deftest shouldReturn200
   (let [response (home {})]
     (testing "success"
       (is (= 200 (:status response))))))
