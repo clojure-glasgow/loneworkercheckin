a33b1d18800444f007791586240f00dd61e13390
Jonathan Abourbih <jabourbi@thoughtworks.com>
2015-02-18 19:47:33 +0000

    Revert "David/Andy Add or remove linked emails"
    This reverts commit 3b8c72c05c68b641ae42d7cefc663d421238790f.

--- test/checkin/email_management_test.clj
+++ test/checkin/email_management_test.clj
@@ -1,11 +1,10 @@
-(ns checkin.email_management_test
-  (:use midje.sweet)
-  (:require [clojure.test :refer :all]
-            ))
+(ns checkin.email-management-test
+  (:require [midje.sweet :refer :all]
+            [checkin.email-management :refer :all]))
 
 
 
-(fact "When I add an email address it should be valid and return 201"
+(future-fact "When I add an email address it should be valid and return 201"
       (add-email "ballbag@example.com") => [201]
       (add-email "ballbag#example.com") => [500]
       )
