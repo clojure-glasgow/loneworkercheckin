7318492e2cd2d8d249d7b53fec40d868b7fac5d6
Robin Weston <lcfcforever@hotmail.com>
2015-02-25 20:15:18 +0000

    fixed namespace error in email tests

--- test/checkin/email_management_test.clj
+++ test/checkin/email_management_test.clj
@@ -1,6 +1,6 @@
 (ns checkin.email-management-test
   (:require [midje.sweet :refer :all]
-            [checkin.email-management :refer :all]))
+            [checkin.email-management :as email]))
 
 (fact "When I add an email address it should be valid and return 201"
       (email/add-email "ballbag@example.com") => [201]
