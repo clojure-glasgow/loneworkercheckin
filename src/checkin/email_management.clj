902c928f65304be60b47571bd17543ab4711a24e
Chris Birch <cbirch@thoughtworks.com>
2015-02-25 19:03:50 +0000

    Andy/Rob email validation returning 201 valid and 500 invalid

--- src/checkin/email_management.clj
+++ src/checkin/email_management.clj
@@ -1,4 +1,10 @@
 (ns checkin.email-management)
 
+  (defn email-valid? [email-address]
+        (re-matches #"\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\b" email-address))
+
 (defn add-email [email-address]
-  )
+  (cond
+    (email-valid? email-address) [201]
+    :else [500]))
+
--- test/checkin/email_management_test.clj
+++ test/checkin/email_management_test.clj
@@ -2,9 +2,14 @@
   (:require [midje.sweet :refer :all]
             [checkin.email-management :refer :all]))
 
+(fact "When I add an email address it should be valid and return 201"
+      (email/add-email "ballbag@example.com") => [201]
 
-
-(future-fact "When I add an email address it should be valid and return 201"
-      (add-email "ballbag@example.com") => [201]
-      (add-email "ballbag#example.com") => [500]
       )
+
+(fact "When I add an email address that is invalid, it should return 500"
+      (email/add-email "blahblah" ) => [500]
+      (email/add-email "ballbag#example.com") => [500]
+      (email/add-email "") => [500]
+      )
+
