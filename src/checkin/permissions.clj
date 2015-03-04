a52863c730f1f78634b9df3c4dbcccaf88cbd23b
Robin Gower <robin.git@infonomics.ltd.uk>
2015-03-04 19:59:09 +0000

    Tony and Robin: Revise permissions to make notify and view-appointments independent

--- src/checkin/permissions.clj
+++ src/checkin/permissions.clj
@@ -1,7 +1,12 @@
 (ns checkin.permissions)
 
+(defn has-permission? [permission permissions-set]
+  (not (nil? (permission permissions-set))))
+
+(defn has-notify-permission? [[email-address permissions-set]]
+  (has-permission? :notify permissions-set))
+
 (defn addresses-to-notify
   [person]
-  (let [friends (:friends person)
-        has-empty-permissions? #(empty? (second %))]
-    (into {} (remove has-empty-permissions? friends))))
+  (let [friends (:friends person)]
+    (into {} (filter has-notify-permission? friends))))
--- test/checkin/permissions_test.clj
+++ test/checkin/permissions_test.clj
@@ -2,13 +2,12 @@
    (:require [midje.sweet :refer :all]
              [checkin.permissions :refer :all]))
 
-(def some-user {:friends {"email1@example.com" #{:include-appointments}
+(def some-user {:friends {"email1@example.com" #{:view-appointments}
                           "email2@example.com" #{}
-                          "email3@example.com" #{:include-appointments}
+                          "email3@example.com" #{:view-appointments :notify}
                           "email4@example.com" #{:notify}}})
 
 (facts "about permissions"
-      (fact "list of people getting notified doesn't include people with empty permissions"
-            (addresses-to-notify some-user) => {"email1@example.com" #{:include-appointments}
-                                                "email3@example.com" #{:include-appointments}
+      (fact "list of people getting notified only includes those with permission"
+            (addresses-to-notify some-user) => {"email3@example.com" #{:view-appointments :notify}
                                                 "email4@example.com" #{:notify}}))
