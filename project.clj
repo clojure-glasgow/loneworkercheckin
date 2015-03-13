c89f6ea7b150eceb6faf4fd0f79b537e49e8ca36
Brydon Cheyney <brydon@remission.org.uk>
2015-03-13 18:08:49 +0000

    Brydon - defined :min-lein-version to force 2.0.0+ as Heroku uses cached lein 1.7.1

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject checkin "0.1.0-SNAPSHOT"
             :description "FIXME: write description"
             :url "http://example.com/FIXME"
+            :min-lein-version "2.0.0"
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.6.0"]
