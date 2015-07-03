0fbdff4882f233f50601933200ab0da7a1c99b0d
Brydon Cheyney <brydon@remission.org.uk>
2015-07-03 21:25:57 +0100

    Brydon - clojure 1.7.0

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
             :min-lein-version "2.0.0"
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.6.0"]
+            :dependencies [[org.clojure/clojure "1.7.0"]
                            [org.slf4j/slf4j-api "1.7.12"]
                            [ch.qos.logback/logback-classic "1.1.3"]
                            [ring "1.3.2"]
